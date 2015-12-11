6cea6c08a610370fd80f73a0293836898dec93d1
Marcos Pirani <marcos.pir94@gmail.com>
2015-12-10 22:34:37 -0300

    agregado de logout

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,22 @@ var passport = module.parent.exports.passport;
 var Employees = require('../models/employees.js');
 var Admins = require('../models/admins.js');
 
+//securizar rutas
+var adminAuth = function(req, res, next){
+    //authorize role
+    if(typeof req.user != "undefined"){
+        next();
+    }else{
+        //Not authorized redirect
+        res.redirect('/');
+    }
+}
+
+app.use(function(req, res, next) {
+    res.locals.user = req.user;
+    next();
+});
+
 app.get('/admin', function(req, res){
     var msg = req.flash('message');
     res.render('admin', { title: 'Login', flashmsg: msg});
@@ -19,20 +35,25 @@ app.post('/admin',function(req, res, next){
       failureFlash: true 
     }));
 
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
 
-app.get('/panel/employees', function(req, res){
+
+app.get('/panel/employees', adminAuth, function(req, res){
     var msg = req.flash('message'); // Read the flash message
     Employees.find({}, function(err, docs){
         res.render('employees', { title: 'Employees', employee: docs, flashmsg: msg}); //Pass the flash message to the view
     });
 });
 
-app.get('/panel/employees/new', function(req, res){
+app.get('/panel/employees/new', adminAuth, function(req, res){
     req.flash('message', 'You added a new employee!'); // Save the flash message in this page
     res.render('new', { title: 'New Employee'});
 });
 
-app.post('/panel/employees/new', function(req, res){
+app.post('/panel/employees/new', adminAuth, function(req, res){
     console.log(req.body);
     var e = new Employees({ name: req.body.name, lastName: req.body.lastName, email: req.body.email });
     e.save(function(err, doc){
@@ -44,7 +65,7 @@ app.post('/panel/employees/new', function(req, res){
     }); 
 });
 
-app.get('/panel/employees/delete/:id', function(req, res){
+app.get('/panel/employees/delete/:id', adminAuth, function(req, res){
     req.flash('message', 'You deleted an employee'); //Save the flash message in this page
     Employees.remove({ _id: req.params.id }, function(err, doc){
         if(!err){
@@ -55,7 +76,7 @@ app.get('/panel/employees/delete/:id', function(req, res){
     });
 });
 
-app.get('/panel/employees/edit/:id', function(req, res){
+app.get('/panel/employees/edit/:id', adminAuth, function(req, res){
     req.flash('message', 'you modified an employee!'); // Save the flash message in this page
     Employees.findOne({ _id: req.params.id }, function(err, doc){
         if(!err){
@@ -66,7 +87,7 @@ app.get('/panel/employees/edit/:id', function(req, res){
     });
 });
 
-app.post('/panel/employees/edit/:id', function(req, res){
+app.post('/panel/employees/edit/:id', adminAuth, function(req, res){
     Employees.findOne({ _id: req.params.id }, function(err, doc){
         if(!err){
             doc.name = req.body.name; 
